a98d9a5ba99b79720ec1c8179e2e9f41128b5bfa
malampati-charan <cmalampati@gmail.com>
2024-07-25 22:24:15 +0530

    Completed Merge Two Sorted Lists -day6

---
+++ merge_sorted_lists-day6.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        //runtime exception with the latter one, let's solve this problem with the dummy node concept
+        ListNode* dummy = new ListNode(0);
+        ListNode* current = dummy;
+
+        //adding elements until one of the lists is exhausted
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1 -> val <= list2 -> val) {
+                current -> next = list1;
+                list1 = list1 -> next;
+            } else {
+                if (list2 -> val <= list1 -> val) {
+                    current -> next = list2;
+                    list2 = list2 -> next;
+                }
+            }
+
+            current = current -> next;
+        }
+
+        //append remaining elements
+        if (list1 != nullptr) {
+            current -> next = list1;
+        } else {
+            current -> next = list2;
+        }
+
+        //reteiving head
+        ListNode* head = dummy -> next;
+        delete dummy;
+        return head;
+    }
+};
